e9f6f7df78620faca05a50beb6f27293c43c35ed
Michael Fitzke <michaelfitzke@gmx.de>
2015-02-08 16:08:55 +0100

    fitting works now

--- logistic_uplift.py
+++ logistic_uplift.py
@@ -7,15 +7,7 @@ from sklearn import linear_model
 import numpy as np
 
 class LogisticUplift(linear_model.LogisticRegression):
-  def __init__(self, penalty='l2', dual=False, tol=1e-4, C=1.0,
-               fit_intercept=True, intercept_scaling=1, class_weight=None,
-               random_state=None, solver='liblinear', max_iter=100,
-               multi_class='ovr', verbose=0):
-
-    
-    super(LogisticUplift,self).__init__(penalty, dual, tol, C, fit_intercept,
-					 intercept_scaling, class_weight, random_state, solver,
-					max_iter, multi_class, verbose)
+ 
 
   def fit(self,X,T,y):
     #setting up Treatment Variables X*T
